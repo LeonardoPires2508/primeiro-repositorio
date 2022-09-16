be9ab3e3ecf6a0c1bdcfed2635b6a7204698ede9
LeonardoPires2508 <ppires.leonardo@gmail.com>
2022-09-16 16:52:57 -0300

    operadores de incremento e decremento

---
+++ index.js
@@ -0,0 +1,15 @@
+/*Tipos Primitivos*/
+console.log('Hello World')
+console.log(5)
+console.log(43.7)
+console.log(true)
+console.log(false)
+
+/*Variaveis*/
+console.log("Estudando Variáveis")
+
+let mensagem = 'Minha mensagem'
+console.log(mensagem)
+
+mensagem = 'Novo Texto'
+console.log(mensagem)
---
+++ operadores_de_incrementos.js
@@ -0,0 +1,20 @@
+alert('Código Funcionando!')
+const nomeUsuario = prompt('Olá usuário! Qual é o seu nome?')
+
+let num = Number(prompt('Olá, ' + nomeUsuario + '. Digite algum número, por favor'))
+//console.log(typeof num)//
+
+//const sucessor = num++
+//const antecessor = num - 1
+
+//alert('o sucessor de ' + num + ' é ' + sucessor)
+//alert('o antecessor de ' + num + ' é ' + antecessor)
+
+console.log(num++)
+console.log(num)
+console.log(++num)
+
+// operadores de decremento
+console.log(num--)
+console.log(num)
+console.log(--num)
